2863d0b29c5d168b595257462ffc0915f33d8223
ahmedreda <ahmedreda7296@gmail.com>
2020-03-22 23:15:22 +0200

    Initial Commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const handlers = require("./handlers");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.json({ hello: "world" }));
 app.use(handlers.notFound);
